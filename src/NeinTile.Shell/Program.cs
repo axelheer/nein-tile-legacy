655bb57a648d1f626a11b2b1fd4f70429612104a
Axel Heer <axel@heer.io>
2020-03-22 11:19:09 +0100

    Fix some warning

--- src/NeinTile.Shell/Program.cs
+++ src/NeinTile.Shell/Program.cs
@@ -6,7 +6,7 @@ namespace NeinTile.Shell
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Strings.HelloWorld);
         }
     }
 }
