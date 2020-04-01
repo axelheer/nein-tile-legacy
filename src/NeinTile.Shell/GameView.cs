d4d042e9dd2ca2495e7fb802170c38fbb55a2c46
Axel Heer <axel@heer.io>
2020-04-01 12:35:54 +0200

    Improve shell on windows

---
+++ src/NeinTile.Shell/GameView.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+#pragma warning disable CA1822
+
+namespace NeinTile.Shell
+{
+    public sealed class GameView : IDisposable
+    {
+        private static bool IsWindows => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        private readonly int bufferWidth;
+        private readonly int bufferHeigth;
+
+        private readonly bool cursorVisible;
+
+        private readonly int windowWidth;
+        private readonly int windowHeight;
+
+        public GameView(int width, int height)
+        {
+            if (IsWindows)
+            {
+                bufferWidth = Console.BufferWidth;
+                bufferHeigth = Console.BufferHeight;
+                cursorVisible = Console.CursorVisible;
+                windowWidth = Console.WindowWidth;
+                windowHeight = Console.WindowHeight;
+
+                Console.SetWindowSize(1, 1);
+                Console.SetBufferSize(width, height + 4);
+                Console.SetWindowSize(width, height + 4);
+                Console.CursorVisible = false;
+            }
+
+            Console.Clear();
+        }
+
+        public void Paint(string content)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write(content);
+        }
+
+        public ConsoleKeyInfo Next()
+            => Console.ReadKey(true);
+
+        public void Nope()
+            => Console.Beep();
+
+        public void Clear()
+            => Console.Clear();
+
+        public void Dispose()
+        {
+            Console.Clear();
+
+            if (IsWindows)
+            {
+                Console.SetWindowSize(1, 1);
+                Console.SetBufferSize(bufferWidth, bufferHeigth);
+                Console.SetWindowSize(windowWidth, windowHeight);
+                Console.CursorVisible = cursorVisible;
+            }
+        }
+    }
+}
--- src/NeinTile.Shell/Program.cs
+++ src/NeinTile.Shell/Program.cs
@@ -35,93 +35,96 @@ namespace NeinTile.Shell
             var gameOptions = new GameOptions(Columns, Rows, Layers);
             var gameState = GameFactory.CreateNew(Edition, gameOptions);
 
-            Console.Clear();
-
-            while(playing)
+            using (var view = new GameView(gamePrinter.Width, gamePrinter.Height))
             {
-                var text = gamePrinter.Print(gameState, layerIndex);
-
-                Console.SetCursorPosition(0, 0);
-                Console.Write(text);
-
-                var next = Console.ReadKey(true);
-                switch (next.Key)
+                while (playing)
                 {
-                    case ConsoleKey.RightArrow when gameState.CanMove(MoveDirection.Right):
-                        gameState = gameState.Move(MoveDirection.Right);
-                        break;
+                    var content = gamePrinter.Print(gameState, layerIndex);
+                    view.Paint(content);
 
-                    case ConsoleKey.LeftArrow when gameState.CanMove(MoveDirection.Left):
-                        gameState = gameState.Move(MoveDirection.Left);
-                        break;
+                    var next = view.Next();
+                    switch (next.Key)
+                    {
+                        case ConsoleKey.RightArrow when gameState.CanMove(MoveDirection.Right):
+                            gameState = gameState.Move(MoveDirection.Right);
+                            break;
 
-                    case ConsoleKey.UpArrow when gameState.CanMove(MoveDirection.Up):
-                        gameState = gameState.Move(MoveDirection.Up);
-                        break;
+                        case ConsoleKey.LeftArrow when gameState.CanMove(MoveDirection.Left):
+                            gameState = gameState.Move(MoveDirection.Left);
+                            break;
 
-                    case ConsoleKey.DownArrow when gameState.CanMove(MoveDirection.Down):
-                        gameState = gameState.Move(MoveDirection.Down);
-                        break;
+                        case ConsoleKey.UpArrow when gameState.CanMove(MoveDirection.Up):
+                            gameState = gameState.Move(MoveDirection.Up);
+                            break;
 
-                    case ConsoleKey.F when gameState.CanMove(MoveDirection.Forward):
-                        gameState = gameState.Move(MoveDirection.Forward);
-                        break;
+                        case ConsoleKey.DownArrow when gameState.CanMove(MoveDirection.Down):
+                            gameState = gameState.Move(MoveDirection.Down);
+                            break;
 
-                    case ConsoleKey.B when gameState.CanMove(MoveDirection.Backward):
-                        gameState = gameState.Move(MoveDirection.Backward);
-                        break;
+                        case ConsoleKey.F when gameState.CanMove(MoveDirection.Forward):
+                            gameState = gameState.Move(MoveDirection.Forward);
+                            break;
 
-                    case ConsoleKey.U when gameState.Previous is { }:
-                        gameState = gameState.Previous;
-                        break;
+                        case ConsoleKey.B when gameState.CanMove(MoveDirection.Backward):
+                            gameState = gameState.Move(MoveDirection.Backward);
+                            break;
 
-                    case ConsoleKey.D1:
-                        layerIndex = 0;
-                        break;
+                        case ConsoleKey.U when gameState.Previous is { }:
+                            gameState = gameState.Previous;
+                            break;
 
-                    case ConsoleKey.D2 when Layers > 1:
-                        layerIndex = 1;
-                        break;
+                        case ConsoleKey.D1:
+                            layerIndex = 0;
+                            break;
 
-                    case ConsoleKey.D3 when Layers > 2:
-                        layerIndex = 2;
-                        break;
+                        case ConsoleKey.D2 when Layers > 1:
+                            layerIndex = 1;
+                            break;
 
-                    case ConsoleKey.D4 when Layers > 3:
-                        layerIndex = 3;
-                        break;
+                        case ConsoleKey.D3 when Layers > 2:
+                            layerIndex = 2;
+                            break;
 
-                    case ConsoleKey.D5 when Layers > 4:
-                        layerIndex = 4;
-                        break;
+                        case ConsoleKey.D4 when Layers > 3:
+                            layerIndex = 3;
+                            break;
 
-                    case ConsoleKey.D6 when Layers > 5:
-                        layerIndex = 5;
-                        break;
+                        case ConsoleKey.D5 when Layers > 4:
+                            layerIndex = 4;
+                            break;
 
-                    case ConsoleKey.D7 when Layers > 6:
-                        layerIndex = 6;
-                        break;
+                        case ConsoleKey.D6 when Layers > 5:
+                            layerIndex = 5;
+                            break;
 
-                    case ConsoleKey.D8 when Layers > 7:
-                        layerIndex = 7;
-                        break;
+                        case ConsoleKey.D7 when Layers > 6:
+                            layerIndex = 6;
+                            break;
 
-                    case ConsoleKey.D9 when Layers > 8:
-                        layerIndex = 8;
-                        break;
+                        case ConsoleKey.D8 when Layers > 7:
+                            layerIndex = 7;
+                            break;
 
-                    case ConsoleKey.D0 when Layers > 9:
-                        layerIndex = 9;
-                        break;
+                        case ConsoleKey.D9 when Layers > 8:
+                            layerIndex = 8;
+                            break;
 
-                    case ConsoleKey.Q when next.Modifiers != default:
-                        playing = false;
-                        break;
+                        case ConsoleKey.D0 when Layers > 9:
+                            layerIndex = 9;
+                            break;
 
-                    default:
-                        Console.Beep();
-                        break;
+                        case ConsoleKey.R:
+                            view.Clear();
+                            break;
+
+                        case ConsoleKey.Q when next.Modifiers != default:
+                            playing = false;
+                            break;
+
+                        default:
+                            view.Nope();
+                            break;
+                    }
                 }
             }
         }
